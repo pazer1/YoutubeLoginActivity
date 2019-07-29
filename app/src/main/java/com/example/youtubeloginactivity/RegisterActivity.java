fa268ab52b4e9202b107b2bfafdfb0ec665d89d3
pazer1 <elven404@gmail.com>
2019-07-29 14:59:18 +0900

    First Commit

--- app/src/main/java/com/example/youtubeloginactivity/RegisterActivity.java
+++ app/src/main/java/com/example/youtubeloginactivity/RegisterActivity.java
@@ -3,13 +3,30 @@ package com.example.youtubeloginactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class RegisterActivity extends AppCompatActivity {
 
     EditText idText,idPassword,idName,idAge;
-    Button regstrtBtn;
+    Button registerBtn;
+    String userId,userPassword,userAge,userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +40,54 @@ public class RegisterActivity extends AppCompatActivity {
         idPassword = findViewById(R.id.register_password);
         idName = findViewById(R.id.register_name);
         idAge = findViewById(R.id.register_age);
-        regstrtBtn = findViewById(R.id.register_btn);
+        registerBtn = findViewById(R.id.register_btn);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userId = idText.getText().toString();
+                userPassword = idText.getText().toString();
+                userAge = idText.getText().toString();
+                userName = idText.getText().toString();
+
+                updateUserInfo();
+            }
+        });
     }
+
+    private void updateUserInfo(){
+        String url = "";
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody formBody = new FormBody.Builder()
+                .add("userId",userId)
+                .add("userPassword",userPassword)
+                .add("userAge",userAge)
+                .add("userName",userName)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(formBody)
+                .build();
+
+        client.newCall(request).enqueue(updateUserinfoCallback);
+    }
+
+    private Callback updateUserinfoCallback = new Callback() {
+        @Override
+        public void onFailure(@NotNull Call call, @NotNull IOException e) {
+            Log.d("Test","Error Message : "+e.getMessage());
+        }
+
+        @Override
+        public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+            try {
+                JSONObject jsonObject = new JSONObject(response.body().toString());
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    };
 }
